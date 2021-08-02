32a1bf94993b936a002f507fc7e614fdce6532a1
James Marotta <james.marotta@infotrack.com.au>
2021-08-02 16:01:13 +1000

    C:/Program Files/Git/titles

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+const express = require("express");
+const app = express();
+const port = 3007;
+app.use(express.json());
+const authUsers = {
+    username: "infotrack",
+    password: "hiring",
+};
+app.get("/api", (req, res) => {
+    res.send(`welcome to infotrack, you can hit /titles if you include the following headers: ${JSON.stringify(authUsers, null, 2)}`);
+});
+app.post("/titles", (req, res) => {
+    if (req.headers.username !== authUsers.username ||
+        req.headers.password !== authUsers.password) {
+        res.status(401).send("Unauthorised, Incorrect headers");
+    }
+    else if (!req.body.titleReference || !req.body.matter) {
+        console.log({ r: req.body });
+        res
+            .status(500)
+            .send({ error: "Title reference and matter are required", r: req.body });
+    }
+    else {
+        console.log({ body: req.body });
+        res.status(200).json({
+            "lot/plan": req.body.titleReference,
+            firstOwner: "Government Prop",
+            LGA: "Sydney",
+            Price: "26.40",
+            matter: req.body.matter,
+        });
+    }
+});
+app.listen(port, () => {
+    console.log(`running on port ${port}`);
+});
--- index.ts
+++ index.ts
@@ -1,14 +1,44 @@
 const express = require("express");
 
 const app = express();
-const port = 4000;
+const port = 3007;
 
-app.get("/auth", (req, res) => {
-  res.send("Well done!");
-});
+app.use(express.json());
+
+const authUsers = {
+  username: "infotrack",
+  password: "hiring",
+};
 
 app.get("/api", (req, res) => {
-  res.send("Well done!");
+  res.send(
+    `welcome to infotrack, you can hit /titles if you include the following headers: ${JSON.stringify(
+      authUsers,
+      null,
+      2
+    )}`
+  );
+});
+
+app.post("/titles", (req, res) => {
+  if (
+    req.headers.username !== authUsers.username ||
+    req.headers.password !== authUsers.password
+  ) {
+    res.status(401).send("Unauthorised, Incorrect headers");
+  } else if (!req.body.titleReference || !req.body.matter) {
+    res
+      .status(500)
+      .send({error: "Title reference and matter are required", body: req.body});
+  } else {
+    res.status(200).json({
+      "lot/plan": req.body.titleReference,
+      firstOwner: "Government Prop",
+      LGA: "Sydney",
+      Price: "26.40",
+      matter: req.body.matter,
+    });
+  }
 });
 
 app.listen(port, () => {
